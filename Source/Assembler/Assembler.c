a17af6eff74980a1f1811741cc19067585c39b34
Anachronaut <75824887+RealBusinessAccount@users.noreply.github.com>
2024-10-26 15:46:09 -0400

    Update Assembler.c

--- Source/Assembler/Assembler.c
+++ Source/Assembler/Assembler.c
@@ -71,14 +71,11 @@ void assemblerCleanup(intermediateElement *intermediateArray, int arraySize, cha
     }
     // Free the intermediateArray itself.
     free(intermediateArray);
-
     // Free the list of included files.
     freeIncludeList();
-
     // Free the list of labels.
     freeLabelList();
-
-    // Free the output file name
+    // Free the output file name.
     free(outputFileName);
 }
 
